5bdd3720a1ece019afd0cb9e58e4e8874e4b0fd0
Павел <Pavel@mail.ru>
2021-03-27 17:57:13 +0400

    app.config.devtools

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-app.config.devtools = true
 
+const app = createApp(App);
+app.config.devtools = true;
 
 createApp(App).mount("#app");
